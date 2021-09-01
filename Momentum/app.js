e27ca54609d18eb486fbc94143aeb856969bc55e
Shaira Nacino <annoraischia@gmail.com>
2021-09-01 17:06:55 +0800

    added html and css to todo

--- Momentum/app.js
+++ Momentum/app.js
@@ -95,3 +95,12 @@ addButton.onclick = function(){
 }
 
 //To-Do
+let addToDoButton = document.getElementById("addToDo");
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById("inputField");
+
+addToDoButton.addEventListener('click', function() {
+  var paragraph = document.createElement('p')
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+})
